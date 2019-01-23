453718ec8e51d5ab71e414d75d524bf019ed3ad0
xushWT <sockenleidenschaft@gmail.com>
2019-01-23 16:51:12 +0100

    rubinum almost working

--- src/lidev/RubinumAccCreator.java
+++ src/lidev/RubinumAccCreator.java
@@ -6,14 +6,14 @@ import com.gargoylesoftware.htmlunit.*;
 import com.gargoylesoftware.htmlunit.html.*;
 
 public class RubinumAccCreator extends AccCreator{
-	private final int USER_LEN = 7;
-	private final int PASS_LEN = 6;
+	private final int USER_LEN = 8;
+	private final int PASS_LEN = 8;
 	
 	public RubinumAccCreator(BrowserVersion browser) {
 		super(browser);
 	}
 
-	public boolean createAccount(String user, String pw) {
+	public boolean createAccount() {
 		try {
 			HtmlPage page = getClient().getPage("https://s1.rubinum.biz/register");
 			HtmlForm form = page.getForms().get(0); // spaghetti but 1 line, apparently rubinum is not able to name their forms... w/e
@@ -30,6 +30,13 @@ public class RubinumAccCreator extends AccCreator{
 			form.getInputByName("password").setValueAttribute(password);
 			form.getInputByName("password_confirmation").setValueAttribute(password);
 			form.getInputByName("security_answer").setValueAttribute(generateRandString(false, 5));
+			
+			HtmlButton button = page.getFirstByXPath("//button[.=\"\n                            Register now !                        \"]"); // yep, the button is apparently labeled this way
+			page = (HtmlPage)button.click();
+			getClient().waitForBackgroundJavaScript(1500);
+			// debugging stuff...
+			System.out.println(page.asText());
+			System.out.println("U: " + username + "\nP: " + password);
 		} catch (FailingHttpStatusCodeException | IOException e) {
 			e.printStackTrace();
 		}
